c735fb77927265ca66c7b1353a7c8bcc60e6f413
Iago Augusto <iagoalvesaugusto@gmail.com>
2022-11-29 20:52:34 -0300

    Bara de Status

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { View, Text, ScrollView ,StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import LifeStatus from "../../Components/Common/LifeStatus"
+import LifeStatus from "../../Components/Common/LifeStatus";
+import StatusBar from '../../Components/Home/StatusBar';
 
 export default function Home(){
   const navigation = useNavigation();
@@ -17,6 +18,7 @@ export default function Home(){
         <View style={{alignItems: "center"}}> 
           <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>  
           <LifeStatus />
+          <StatusBar />
         </View>
         <Text style={styles.explanationText}
           onPress={() => {
